f450af58e5d01268a3a5fb037c3251ee7f62c535
arata-1972 <arata1972.info@gmail.com>
2016-11-16 12:53:25 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-    databaseURI: databaseUri || 'mongodb://heroku_4wr3p8c1:5pq3k7ij82sikdpkbfjaa31mk@ds147797.mlab.com:47797/heroku_4wr3p8c1',
+    databaseURI: databaseUri || 'mongodb://heroku_b3x07tz1:76e1qob73tm39flto2opud4i8o@ds019946.mlab.com:19946/heroku_b3x07tz1',
     cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-    appId: process.env.APP_ID || 'yve80c5d',
-    masterKey: process.env.MASTER_KEY || 'wvpy3f1k', //Add your master key here. Keep it secret!
-    serverURL: process.env.SERVER_URL || 'https://dictionary-1.herokuapp.com/parse', // Don't forget to change to https if needed
+    appId: process.env.APP_ID || 'T0blCo13DopfPeuIcQZdqC3C518UvetlZZcrOQhi',
+    masterKey: process.env.MASTER_KEY || 'T6ivq9iWh1LVLpHjtdGmYyvJnE5bG3kWxvIcb4Xp', //Add your master key here. Keep it secret!
+    serverURL: process.env.SERVER_URL || 'https://japanese-contonese.herokuapp.com/parse', // Don't forget to change to https if needed
     liveQuery: {
         classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
     },
@@ -28,38 +28,38 @@ var api = new ParseServer({
         //   apiKey: '...'
         // },
         ios: [{
-            pfx: __dirname + '/iPhoneP12/com.arata1972.arabic.french.dict store.p12',
-            bundleId: 'com.arata1972.arabic.french.dict',
+              pfx: __dirname + '/iPhoneP12/com.arata1972.chinese.indonesian.dict store.p12',
+            bundleId: 'com.arata1972.chinese.indonesian.dict',
             production: true
         }, {
 
-            pfx: __dirname + '/iPhoneP12/com.arata1972.persian.french.dict store.p12',
-            bundleId: 'com.arata1972.persian.french.dict',
+             pfx: __dirname + '/iPhoneP12/com.arata1972.english.french.dict store.p12',
+            bundleId: 'com.arata1972.english.french.dict',
             production: true
         }, {
-            pfx: __dirname + '/iPhoneP12/com.arata1972.japanese.italian.dict store.p12',
-            bundleId: 'com.arata1972.japanese.italian.dict',
+            pfx: __dirname + '/iPhoneP12/com.arata1972.japanese.english.dict store.p12',
+            bundleId: 'com.arata1972.japanese.english.dict',
             production: true
         }, {
 
-            pfx: __dirname + '/iPhoneP12/com.arata1972.chinese.lao.dict store.p12',
-            bundleId: 'com.arata1972.chinese.lao.dict',
+              pfx: __dirname + '/iPhoneP12/com.arata1972.japanese.thai.dict store.p12',
+            bundleId: 'com.arata1972.japanese.thai.dict',
             production: true
         }, {
-            pfx: __dirname + '/iPhoneP12/com.arata1972.thai.koren.dict store.p12',
-            bundleId: 'com.arata1972.thai.koren.dict',
+              pfx: __dirname + '/iPhoneP12/com.arata1972.japanese.korean.dict store.p12',
+            bundleId: 'com.arata1972.japanese.korean.dict',
             production: true
         }, {
-            pfx: __dirname + '/iPhoneP12/com.arata1972.chinese.nepali.dict store.p12',
-            bundleId: 'com.arata1972.chinese.nepali.dict',
+                pfx: __dirname + '/iPhoneP12/com.arata1972.english.thai.dict store.p12',
+            bundleId: 'com.arata1972.english.thai.dict',
             production: true
         }, {
-            pfx: __dirname + '/iPhoneP12/com.arata1972.japanese.tagalog.dict store.p12',
-            bundleId: 'com.arata1972.japanese.tagalog.dict',
+            pfx: __dirname + '/iPhoneP12/com.satoshogo.SimpleJapaneseContonese store.p12',
+            bundleId: 'com.satoshogo.SimpleJapaneseContonese',
             production: true
         }, {
-            pfx: __dirname + '/iPhoneP12/com.arata1972.japanese.indonesian.dict store.p12',
-            bundleId: 'com.arata1972.japanese.indonesian.dict',
+            pfx: __dirname + '/iPhoneP12/com.satoshogo.SimpleEnglishContonese store.p12',
+            bundleId: 'com.satoshogo.SimpleEnglishContonese',
             production: true
         }]
     },
