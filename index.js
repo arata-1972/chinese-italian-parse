6f08b80c137b91f1293ce02ad1be953846171dd8
arata-1972 <arata1972.info@gmail.com>
2016-11-16 12:22:47 +0900

    Update index.js

--- index.js
+++ index.js
@@ -12,31 +12,58 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://heroku_mzh9z7fj:2biadrg17pg08u1e1p1qvqa7jt@ds023432.mlab.com:23432/heroku_mzh9z7fj',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'PJGh686wADXdRboLLmbkER0V4D95wlLVatRJXUl0',
-  masterKey: process.env.MASTER_KEY || '4t5Y7WU5lShBRzNz4SeT1gbXxqfABGRvH0T8bL3c', //Add your master key here. Keep it secret!
-  serverURL: process.env.SERVER_URL || 'https://night-market--taiwan.herokuapp.com/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  },
-  // 以下為新增部分
-  push: {
-    // 此篇未提到 Android，因此註解掉
-    // android: {
-    //   senderId: '...',
-    //   apiKey: '...'
-    // },
-    ios: {
-      pfx:  __dirname + '/iPhoneP12/com.arata1972.night.market.store.p12', // 與 index.js 目錄同層
-      bundleId: 'com.arata1972.night.market', // 填入先前填的 Bundle ID
-      production: true // false: development, true: production
-    }
-  }
+    databaseURI: databaseUri || 'mongodb://heroku_4wr3p8c1:5pq3k7ij82sikdpkbfjaa31mk@ds147797.mlab.com:47797/heroku_4wr3p8c1',
+    cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+    appId: process.env.APP_ID || 'yve80c5d',
+    masterKey: process.env.MASTER_KEY || 'wvpy3f1k', //Add your master key here. Keep it secret!
+    serverURL: process.env.SERVER_URL || 'https://dictionary-1.herokuapp.com/parse', // Don't forget to change to https if needed
+    liveQuery: {
+        classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    },
+    // 以下為新增部分
+    push: {
+        // 此篇未提到 Android，因此註解掉
+        // android: {
+        //   senderId: '...',
+        //   apiKey: '...'
+        // },
+        ios: [{
+            pfx: __dirname + '/iPhoneP12/com.arata1972.arabic.french.dict store.p12',
+            bundleId: 'com.arata1972.arabic.french.dict',
+            production: true
+        }, {
+
+            pfx: __dirname + '/iPhoneP12/com.arata1972.persian.french.dict store.p12',
+            bundleId: 'com.arata1972.persian.french.dict',
+            production: true
+        }, {
+            pfx: __dirname + '/iPhoneP12/com.arata1972.japanese.italian.dict store.p12',
+            bundleId: 'com.arata1972.japanese.italian.dict',
+            production: true
+        }, {
+
+            pfx: __dirname + '/iPhoneP12/com.arata1972.chinese.lao.dict store.p12',
+            bundleId: 'com.arata1972.chinese.lao.dict',
+            production: true
+        }, {
+            pfx: __dirname + '/iPhoneP12/com.arata1972.thai.koren.dict store.p12',
+            bundleId: 'com.arata1972.thai.koren.dict',
+            production: true
+        }, {
+            pfx: __dirname + '/iPhoneP12/com.arata1972.chinese.nepali.dict store.p12',
+            bundleId: 'com.arata1972.chinese.nepali.dict'
+            production: true
+        }, {
+            pfx: __dirname + '/iPhoneP12/com.arata1972.japanese.tagalog.dict store.p12',
+            bundleId: 'com.arata1972.japanese.tagalog.dict',
+            production: true
+        }, {
+            pfx: __dirname + '/iPhoneP12/com.arata1972.japanese.indonesian.dict store.p12',
+            bundleId: 'com.arata1972.japanese.indonesian.dict',
+            production: true
+        }]
+    },
 });
-
-
-
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
